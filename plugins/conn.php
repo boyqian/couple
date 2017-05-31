cf1aa5d483d74d9988c6600cb9801f7976e2d753
boyqian <boyqian@foxmail.com>
2017-05-31 16:51:42 +0800

    plugins

---
+++ plugins/conn.php
@@ -0,0 +1,4 @@
+<?php 
+ $link=mysql_connect('localhost','root','boyqian');
+ mysql_select_db('img');
+ mysql_query("set names 'utf8'");
---
+++ plugins/contact-form.php
@@ -0,0 +1,17 @@
+﻿<?php
+require_once "conn2.php";
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+if($name==""||$eamil==""||$subject==""||$message==""){
+echo "<script>alert('留言失败，请重新提交');window.location.href='../contact.html';</script>";
+}else{
+$sql="INSERT INTO tb_contact SET name='$name',email='$email',subject='$subject',message='$message'";
+if (mysql_query($sql)) {
+	echo "<script>alert('留言成功');window.location.href='../contact.html';</script>";
+
+}else{
+	echo "<script>alert('留言失败，请重新提交');window.location.href='../contact.html';</script>";
+}
+}
